40d2591dc6d8af8f1af3ea787b42eaff351a42fe
RicieriPasqualotto <111090520+Riciler@users.noreply.github.com>
2022-08-15 18:59:33 -0300

    Captação do erro de divisão por zero
    entrada do valor a contido em um loop que não permite o valor 0 para a, assim evitando a divisão por 0 na bhaskara

--- Bhaskara.py
+++ Bhaskara.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug 11 16:54:38 2022
-Version 0.1
+Version 1.1
 @author: Ricieri
 Desc: Calcula as raizes de uma equação da forma ax**2+bx+c=0
-To do: Captação de erros
+To do: Captação de erros no modulo Bhaskara
 """
 import ModuloEstatist
 barrav = 5 * " "
@@ -13,10 +13,18 @@ print(15 * barra)
 print(5 * barra,"Bhaskara resolvimentos", 5 * barra)
 print(15 * barra)
 #%%    Entrada dos valores de a, b, c (da forma ax**2+bx+c=0)
+while True:
+    try:
+        while True:
+            a = int(input((5 * barrav) + ("Digite o Valor de a:     ")))
+            if a == 0: print("Valor Invalido!")
+            else: break
+        b = int(input((5 * barrav) + "Digite o Valor de b:     "))
+        c = int(input((5 * barrav) + "Digite o Valor de c:     "))
+    except ValueError:
+        print((5 * barrav) + "-Entrada Inválida.-")
+    break
 
-a = int(input((5 * barrav) + "Digite o Valor de a:     "))
-b = int(input((5 * barrav) + "Digite o Valor de b:     "))
-c = int(input((5 * barrav) + "Digite o Valor de c:     "))
 #%%    Decoration
 print(15 * barra)
 print(5 * barra,"Resolvente", 7 * barra)
@@ -33,3 +41,12 @@ print((5 * barrav) + f"{conj_s[3]}")
 print(15 * barra)
 print(5 * barra,"Acabô!", 8 * barra)
 print(15 * barra)
+
+#%% Segura o programa no final para leitura
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+input("Aperte qualquer tecla para finalizar o programa.")
